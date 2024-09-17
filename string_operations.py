2afcca8df3268887e0e03e0f75aa51d694666f87
NanaCasmana <awnana123@gmail.com>
2024-09-17 10:54:35 +0700

    repetition + slicing

--- string_operations.py
+++ string_operations.py
@@ -3,4 +3,12 @@ print("Hello" + "World")
 a = "Hello"
 b = "world!"
 
-print(a+b)
+print(a+b)  # Hello world!
+
+# Repetition
+print('halo'*5)  # halohalohalohalohalo
+print('wk'*5)  # wkwkwkwkwk
+
+# Slicing
+nama = 'Nanaz'
+print(nama[1:4])  # ana
