e6403a342d6a99e6cedb66db2574d270f2c336aa
NanaCasmana <awnana123@gmail.com>
2024-09-17 11:36:35 +0700

    add dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# Dictionary
+student = {
+    'name': 'Nanaz',
+    'age': 21,
+    'major': 'Computer Science',
+}
+student['graduation year'] = 2024
+student['age'] = 22
+del student['major']
+
+print(student)
